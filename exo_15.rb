038610e3803b1856560973614c60fa48d9bca6d8
Xuan102 <xuan102emma@gmail.com>
2020-01-14 20:16:51 +0100

    exo15

---
+++ exo_15.rb
@@ -0,0 +1,11 @@
+puts "QUel est votre annee de nassaice?"
+print"> "
+
+annee="gets.chomp.to_i"
+
+while annee !=2020
+    puts "en #{annee}, vous aviez #{age} ans."
+    annee +=1
+    age +=1
+end
+# faut mettre age = 0? 
