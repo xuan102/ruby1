1253a5911f982c6471838306a343448cd59d2b5c
Xuan102 <xuan102emma@gmail.com>
2020-01-15 09:52:36 +0100

    exo15 avec version facile

--- exo_15.rb
+++ exo_15.rb
@@ -9,3 +9,11 @@ while annee !=2020
     age +=1
 end
 # faut mettre age = 0? 
+#================================
+#version facile
+#print "annee?"
+#annee=gets.chomp.to_i
+#until annee > 2020
+#puts annee
+#annee += 1
+#end
